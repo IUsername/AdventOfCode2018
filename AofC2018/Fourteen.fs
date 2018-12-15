b8f0b9a86922bee6351c56e1d90e8fc9e3a880e8
IUsername <benjamin.v.palmer@gmail.com>
2018-12-14 20:37:10 -0700

    Day 14, part 2.

--- AofC2018/Fourteen.fs
+++ AofC2018/Fourteen.fs
@@ -14,6 +14,14 @@ let private toDigitList (n:int) =
         | _ -> loop ((s % 10)::acc) s'
     loop [] n
 
+let private toDigitList8 (n:int) =
+    let rec loop (acc:int8 list) (s:int) = 
+        let s' = s / 10
+        match s' with
+        | 0 -> ((int8 s)::acc) 
+        | _ -> loop ((int8 (s % 10))::acc) s'
+    loop [] n
+
 let private toBigInt (ls:int list) =
     ls |> List.rev |> List.mapi (fun i d -> bigint.Pow(bigint 10,i) * bigint d) |> List.sum
 
@@ -129,14 +137,42 @@ let getCountBeforeMatch (toMatch:int) (state:ElfState,ls:int list) =
     let (state'',_) = matchEnd endL (state',ls)
     state''.count - endL.Length
 
+let areEqual (a:seq<'t>) (b:seq<'t>) =
+    Seq.zip a b |> Seq.forall (fun (i,j) -> i = j)
+
+let gen (d:int) = 
+    let mutable scores = List<int8>([3y;7y])
+    let mutable elf1 = 0
+    let mutable elf2 = 1
+    let mutable len = scores.Count
+    let mutable working = true
+    let mutable shift = 0
+    let toMatch = List<int8>(toDigitList8(d))
+    let matchCount = toMatch.Count
+    while working do
+        let v1 = scores.[elf1]
+        let v2 = scores.[elf2]      
+        scores.AddRange (toDigitList8 (int(v1+v2)))
+        len <- scores.Count
+        elf1 <- (elf1 + int v1 + 1) % len
+        elf2 <- (elf2 + int v2 + 1) % len
+        let index = len-matchCount
+        if index > 1 then
+            let endRange = scores.GetRange(index-1, matchCount)
+            working <- not (areEqual toMatch endRange )
+            if working then
+                let endRange = scores.GetRange(index, matchCount)
+                working <- not (areEqual toMatch endRange )    
+            else
+                shift <- -1
+    scores.Count - matchCount + shift
+
 let execute = fun d ->
     let list = [3;7]
     let state = {aIndex=0;bIndex=1;aValue=3;bValue=7;count=2;pe=[-1]}
     let r = getScoresAfter d 10 (state,list)
     printfn "%A" r
-
-   
-    let r2 = getCountBeforeMatch d (state,list)
+    let r2 = (gen d)
     printfn "%A" r2
 
 let input = 652601
